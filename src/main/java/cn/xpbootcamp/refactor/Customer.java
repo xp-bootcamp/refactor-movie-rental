a38dda2b7059e226fa557c9623481e4d38f62304
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-02 14:08:13 +0800

    Reorgnize and refactor mehtods and parameters

--- src/main/java/cn/xpbootcamp/refactor/Customer.java
+++ src/main/java/cn/xpbootcamp/refactor/Customer.java
@@ -7,6 +7,8 @@ public class Customer {
 
     private String customerName;
     private Vector<Rental> rentals = new Vector<>();
+    private double totalAmount;
+    private int frequentRenterPoints;
 
     Customer(String customerName) {
         this.customerName = customerName;
@@ -16,51 +18,31 @@ public class Customer {
         rentals.addElement(rental);
     }
 
-    public String getCustomerName() {
-        return customerName;
-    }
-
     String statement() {
-        double totalAmount = 0d;
-        int frequentRenterPoints = 0;
-        Enumeration<Rental> rentals = this.rentals.elements();
-        StringBuilder statementResult = new StringBuilder("Rental Record for " + getCustomerName() + "：\n");
-        while (rentals.hasMoreElements()) {
-            Rental each = rentals.nextElement();
-            //show figures for this rental
-            //determine amounts for each line
-            double thisAmount = each.getRentalAmount();
-            switch (each.getMovie().getMovieType()) {
-                case Movie.HISTORY:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CAMPUS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            //add frequent renter points
-            frequentRenterPoints++;
-            if ((each.getMovie().getMovieType() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+        StringBuilder statementResult = buildHeader();
+        buildBody(this, statementResult);
+        buildFooter(totalAmount, frequentRenterPoints, statementResult);
 
-            //show figures for this rental
-            statementResult.append("\t")
-                  .append(each.getMovie().getMovieTitle())
-                  .append("\t")
-                  .append(thisAmount).append("\n");
-            totalAmount += thisAmount;
-        }
-        //add footer lines
-        statementResult.append("Amount owed is ").append(totalAmount).append("\n");
-        statementResult.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return statementResult.toString();
     }
 
+    private static void buildBody(Customer customer, StringBuilder statementResult) {
+        Enumeration<Rental> rentals = customer.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental rental = rentals.nextElement();
+            rental.buildRentalAmount(statementResult);
+
+            customer.frequentRenterPoints = rental.addFrequentRenterPoint(customer.frequentRenterPoints);
+            customer.totalAmount += rental.getRentalAmount();
+        }
+    }
+
+    private StringBuilder buildHeader() {
+        return new StringBuilder("Rental Record for " + this.customerName + "：\n");
+    }
+
+    private void buildFooter(double totalAmount, int frequentRenterPoints, StringBuilder statementResult) {
+        statementResult.append("Amount owed is ").append(totalAmount).append("\n");
+        statementResult.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
+    }
 }
--- src/main/java/cn/xpbootcamp/refactor/Rental.java
+++ src/main/java/cn/xpbootcamp/refactor/Rental.java
@@ -2,7 +2,7 @@ package cn.xpbootcamp.refactor;
 
 public class Rental {
 
-    private Movie movie;
+    Movie movie;
     private int daysRented;
     private double rentalAmount;
 
@@ -11,19 +11,44 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
-    Movie getMovie() {
-        return movie;
-    }
-
-    int getDaysRented() {
-        return daysRented;
-    }
-
-    public double getRentalAmount() {
+    double getRentalAmount() {
         return rentalAmount;
     }
 
-    public void setRentalAmount(double rentalAmount) {
-        this.rentalAmount = rentalAmount;
+    void calculateRentalAmount() {
+        switch (movie.getMovieType()) {
+            case Movie.HISTORY:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case Movie.CAMPUS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+    }
+
+    int addFrequentRenterPoint(int frequentRenterPoints) {
+        frequentRenterPoints++;
+        if ((movie.getMovieType() == Movie.NEW_RELEASE) && daysRented > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
+    void addRentalFigure(StringBuilder statementResult) {
+        statementResult.append("\t")
+              .append(movie.getMovieTitle())
+              .append("\t")
+              .append(rentalAmount).append("\n");
+    }
+
+    void buildRentalAmount(StringBuilder statementResult) {
+        calculateRentalAmount();
+        addRentalFigure(statementResult);
     }
 }
