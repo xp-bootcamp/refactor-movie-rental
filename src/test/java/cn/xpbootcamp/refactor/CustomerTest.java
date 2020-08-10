174f19ac8dd99b99997b187fe6a4ec76ce5d48db
QingQing Dou <qqdou@thoughtworks.com>
2020-08-10 15:16:57 +0800

    add more test case
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/test/java/cn/xpbootcamp/refactor/CustomerTest.java
+++ src/test/java/cn/xpbootcamp/refactor/CustomerTest.java
@@ -8,6 +8,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CustomerTest {
 
     @Test
+    void should_return_correct_result_when_call_statement_given_campus_movie_rent_3_days() {
+        Customer customer = new Customer("lisa");
+        customer.addRental(new Rental(new Movie("同桌的你", Movie.CAMPUS), 2));
+
+        assertThat("Rental Record for lisa：\n" +
+                "\t同桌的你\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points").isEqualTo(customer.statement());
+    }
+
+    @Test
+    void should_return_correct_result_when_call_statement_given_history_movie_rent_2_days() {
+        Customer customer = new Customer("lisa");
+        customer.addRental(new Rental(new Movie("战狼2", Movie.HISTORY), 2));
+
+        assertThat("Rental Record for lisa：\n" +
+                "\t战狼2\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points").isEqualTo(customer.statement());
+    }
+
+    @Test
+    void should_return_correct_result_when_call_statement_given_history_movie_rent_1_day() {
+        Customer customer = new Customer("lisa");
+        customer.addRental(new Rental(new Movie("宠爱", Movie.NEW_RELEASE), 1));
+
+        assertThat("Rental Record for lisa：\n" +
+                "\t宠爱\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points").isEqualTo(customer.statement());
+    }
+
+    @Test
     void should_return_correct_result_when_call_statement() {
         Customer customer = new Customer("老王");
         customer.addRental(new Rental(new Movie("同桌的你", Movie.CAMPUS), 4));
