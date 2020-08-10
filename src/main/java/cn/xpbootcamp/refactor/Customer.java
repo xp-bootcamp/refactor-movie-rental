cab078efd6cf4c9a86e10b241ab7c79459293213
QingQing Dou <qqdou@thoughtworks.com>
2020-08-10 16:06:55 +0800

    refactor state receipt method
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/main/java/cn/xpbootcamp/refactor/Customer.java
+++ src/main/java/cn/xpbootcamp/refactor/Customer.java
@@ -21,20 +21,25 @@ public class Customer {
     }
 
     String stateReceipt() {
-        double totalAmount = 0d;
-        int renterPoints = 0;
-        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "：\n");
-        for (Rental rental : rentals) {
-            renterPoints += rental.countPoints();
-            double thisAmount = rental.amountFor();
-            result.append(rental.buildRow());
-            totalAmount += thisAmount;
-        }
-        result.append(buildReceiptFooter(totalAmount, renterPoints));
-        return result.toString();
+        return "Rental Record for " + getName() + "：\n" +
+                generateReceiptBody() +
+                generateReceiptFooter(calculateTotalAmount(), calculateTotalPoints());
     }
 
-    private StringBuilder buildReceiptFooter(double totalAmount, int frequentRenterPoints) {
+    private StringBuilder generateReceiptBody() {
+        StringBuilder result = new StringBuilder();
+        return rentals.stream().map(Rental::buildRow).reduce(result, StringBuilder::append);
+    }
+
+    private int calculateTotalPoints() {
+        return rentals.stream().mapToInt(Rental::countPoints).sum();
+    }
+
+    private double calculateTotalAmount() {
+        return rentals.stream().mapToDouble(Rental::amountFor).sum();
+    }
+
+    private StringBuilder generateReceiptFooter(double totalAmount, int frequentRenterPoints) {
         StringBuilder result = new StringBuilder();
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
