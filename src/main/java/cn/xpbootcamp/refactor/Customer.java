fe3e68f8cbbf907d08c91af88ac89647cf82bea8
QingQing Dou <qqdou@thoughtworks.com>
2020-08-10 15:19:26 +0800

    use list insteaof vetor
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/main/java/cn/xpbootcamp/refactor/Customer.java
+++ src/main/java/cn/xpbootcamp/refactor/Customer.java
@@ -1,19 +1,19 @@
 package cn.xpbootcamp.refactor;
 
-import java.util.Enumeration;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
 
-    private String name;
-    private Vector<Rental> rentals = new Vector<>();
+    private final String name;
+    private final List<Rental> rentals = new ArrayList<>();
 
     Customer(String name) {
         this.name = name;
     }
 
     void addRental(Rental rental) {
-        rentals.addElement(rental);
+        rentals.add(rental);
     }
 
     public String getName() {
@@ -23,38 +23,34 @@ public class Customer {
     String statement() {
         double totalAmount = 0d;
         int frequentRenterPoints = 0;
-        Enumeration<Rental> rentals = this.rentals.elements();
         StringBuilder result = new StringBuilder("Rental Record for " + getName() + "：\n");
-        while (rentals.hasMoreElements()) {
-            Rental each = rentals.nextElement();
-            //show figures for this rental
-            //determine amounts for each line
+        for (Rental rental : rentals) {
             double thisAmount = 0d;
-            switch (each.getMovie().getPriceCode()) {
+            switch (rental.getMovie().getPriceCode()) {
                 case Movie.HISTORY:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    if (rental.getDaysRented() > 2)
+                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
+                    thisAmount += rental.getDaysRented() * 3;
                     break;
                 case Movie.CAMPUS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    if (rental.getDaysRented() > 3)
+                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
                     break;
             }
             //add frequent renter points
             frequentRenterPoints++;
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
 
             //show figures for this rental
             result.append("\t")
-                  .append(each.getMovie().getTitle())
-                  .append("\t")
-                  .append(thisAmount).append("\n");
+                    .append(rental.getMovie().getTitle())
+                    .append("\t")
+                    .append(thisAmount).append("\n");
             totalAmount += thisAmount;
         }
         //add footer lines
